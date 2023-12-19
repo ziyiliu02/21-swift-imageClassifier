5e0ce819f8e17d683fbb14c5e878362722ef5278
ziyiliu02 <ziyi.liu02@gmail.com>
2023-12-19 07:49:58 +0700

    Initial Commit

---
+++ 21-swift-imageClassifier/_1_swift_imageClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_swift_imageClassifierApp.swift
+//  21-swift-imageClassifier
+//
+//  Created by Liu Ziyi on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct _1_swift_imageClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
